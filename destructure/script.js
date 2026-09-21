74d2b0a7471d7d3f2e95048924ade61a42c820a4
Subrahmanya <codewithsubrahmanya@gmail.com>
2026-09-21 19:31:47 +0530

    done

---
+++ destructure/script.js
@@ -0,0 +1,22 @@
+// let arr = [1, 3, 4, 5, 6, 7];
+// let [a, b, c, d, e, f] = arr;
+// console.log(a);
+// let arr1 = [3, 4, 5, 4, [5, 5, 6]];
+// let [g, h, i, j, [k, l, m]] = arr1;
+// console.log(m);
+
+let arr2 = [10, 20, [30, 40, [function () { return "hii hello" }, [true, [0, [false], "machi"], ["bye", "macha"], 100], 200, 1000], 2000], 50]
+let [a, b, [c, d, [e, [f, [g, [h], i], [j, k], l], m, n], o], p]=arr2;
+console.log(a)
+console.log(g);
+console.log(f);
+console.log(k);
+console.log(m);
+
+console.log(l);
+console.log(h);
+
+
+
+
+
