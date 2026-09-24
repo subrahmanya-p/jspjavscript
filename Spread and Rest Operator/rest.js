c98e3c63d494026cd1d21007e755baf6ec8f71df
Subrahmanya <codewithsubrahmanya@gmail.com>
2026-09-24 19:46:28 +0530

    done

--- Spread and Rest Operator/rest.js
+++ Spread and Rest Operator/rest.js
@@ -1,1 +1,23 @@
-console.log("heloo");
+let arr = [
+    1, 2, 3, 4, 4, 5, 5, 6, 6
+]
+let [a, ...b] = arr;
+console.log(b);
+
+let obj = {
+    name: "subramuayaa",
+    age: 24
+    , salary: 67
+    ,
+    adresss:{
+        city:"subrahmayaa",
+        country:["India","china", "russia"]
+    }
+
+}
+console.log("--------------");
+
+let { name,age,salary,...data }=obj;
+console.log(name);
+console.log(data);
+
